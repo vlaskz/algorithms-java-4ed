64b9e230336f369928d1ca9e84ee5ccb05d7d170
Vlaskz <sam.velasquez@hotmail.com>
2021-06-20 10:58:13 -0300

    added explanation and solution for c1ex111 and c1ex112.

--- src/main/java/com.vlaskz.algorithms/Main.java
+++ src/main/java/com.vlaskz.algorithms/Main.java
@@ -4,5 +4,6 @@ import com.vlaskz.algorithms.exercises.Chapter1;
 public class Main {
     public static void main(String[] args) {
         Chapter1.ex111();
+        Chapter1.ex112();
     }
 }
--- src/main/java/com/vlaskz/algorithms/exercises/Chapter1.java
+++ src/main/java/com/vlaskz/algorithms/exercises/Chapter1.java
@@ -6,18 +6,43 @@ public class Chapter1 {
         /*
             Explanation:
             a) 0+15/2 exact result should return 7.5, but because of int typing, result was truncated to 7;
-            b) 2.0e-6 = 2.0(double) x 10^-6(double 0.000001) = 0.000002 * 100000000.1 = 200.0000002
-                floating-point operations should consider the maximum precision possibility;
+
+            b) 2.0e-6 = 2.0(double) x 10^-6(double 0.000001) = 0.000002 * 100000000.1 = 200.0000002. Floating-point
+                operations should consider the maximum precision possibility;
+
             c) true AND false = false. true AND true result true. False OR true result true;
          */
-
+        System.out.println("Chapter 01 - Exercise 01");
+        //a)
         System.out.println((0+15)/2);
+        //b)
         System.out.println(2.0e-6 * 100000000.1);
+        //c)
         System.out.println(true && false || true && true);
+
+        System.out.println(" ");
     }
 
     public static void ex112(){
+        /*
+            **Note: Object class was used to force type inference by compiler. The method getClass().getSimpleName returns
+                the RUNTIME type inferred by JVM.**
 
+            a) (1+2.236)/2 results in a double, because in the sum, the result is 3.236, thus becoming a floating-point
+                number. Also, the final result is a division of double/int, which results in double with value 1.618.
+
+            b) Same case here. When we have arithmetical operations involving numerical types, the compiler always cast
+                the result to most comprehensive type. In this case, is double.
+         */
+        System.out.println("Chapter 01 - Exercise 02");
+        //a)
+        Object v1 = (1+2.236)/2;
+        System.out.println("Type: " + v1.getClass().getSimpleName() + " Value: "+ v1);
+        //b)
+        Object v2 = 1+2+3+4.0;
+        System.out.println("Type " + v2.getClass().getSimpleName() + " Value: " + v2);
+
+        System.out.println(" ");
 
     }
 }

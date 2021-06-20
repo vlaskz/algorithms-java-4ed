8206afc2d26f6a41e3088ab3dcf610d06f585c4e
Vlaskz <sam.velasquez@hotmail.com>
2021-06-20 10:05:32 -0300

    Initial commit

---
+++ src/main/java/com.vlaskz.algorithms/Main.java
@@ -0,0 +1,8 @@
+package com.vlaskz.algorithms;
+import com.vlaskz.algorithms.exercises.Chapter1;
+
+public class Main {
+    public static void main(String[] args) {
+        Chapter1.ex111();
+    }
+}
---
+++ src/main/java/com/vlaskz/algorithms/exercises/Chapter1.java
@@ -0,0 +1,10 @@
+package com.vlaskz.algorithms.exercises;
+
+public class Chapter1 {
+
+    public static void ex111(){
+        System.out.println((0+15)/2);
+        System.out.println(2.0e-6 * 100000000.1);
+        System.out.println(true && false || true && true);
+    }
+}
